a88967ad10e7234ba95af302f17804b73b112ad7
harrelfe <harrelfe@b307fd46-ab21-0410-8f3d-bb824a5a1080>
2004-08-14 23:47:12 +0000

    LaTeX output to gentex/, new argument continuous= to pass to summary.formula
    git-svn-id: svn+ssh://biostat.mc.vanderbilt.edu/usr/local/svn_repositories/rreport@30 b307fd46-ab21-0410-8f3d-bb824a5a1080

--- trunk/R/mixedvarReport.s
+++ trunk/R/mixedvarReport.s
@@ -4,90 +4,92 @@ mixedvarReport <- function(data, vars, panel, treat,
                            cdf=FALSE, Ohist=TRUE,
                            bpPrototype=FALSE, digits=3, append=FALSE,
                            Major=NULL, MajorLabel='',
-                           Majorvars=NULL, cexMajor=.7,
-                           ...) {
+                           Majorvars=NULL, cexMajor=.7, continuous=10,
+                           pl=TRUE, ...) {
  
   vars  <- unlist(vars)
   Treat <- data[[treat]]
 
   form <- as.formula(paste('Treat', paste(vars,collapse='+'), sep='~'))
-  d <- summary(form, data=data, method='reverse', test=TRUE)
+  d <- summary(form, data=data, method='reverse', test=TRUE, continuous=continuous)
   lp <-  paste(toupper(substring(longPanel,1,1)),
                substring(longPanel,2), sep='')
   latex(d, prtest='P', digits=digits,
-        file=paste(panel, 'tex', sep='.'),
+        file=paste('gentex/',panel, '.tex', sep=''),
         append=append, middle.bold=TRUE,
         caption=paste(lp,'variables'), where='hbp!', ctable=TRUE)
-  if(any(d$type == 1)) {
-    pn <- paste(panel, 'cat', sep='-')
-    startPlot(pn, h=5, w=6) # was h=5 w=6
-    plot(d, which='cat', main='', ...)
-    Key(.8, .02)  #.075)
-    endPlot()
-    putFig(panel, pn,
-           paste('Distribution of categorical',longPanel,'variables'),
-           paste('Distribution of categorical',longPanel,'variables.',
-                 'Proportions on the $x$-axis indicate the',
-                 'treatment-specific proportion of subjects in',
-                 'the category shown on the $y$-axis.'))
-  }
-  if(any(d$type == 2) || length(Major)) {
-    if(bpPrototype) {
-      startPlot('bpplot-prototype', h=4)
-      bpplt()
+  if(pl) {
+    if(any(d$type == 1)) {
+      pn <- paste(panel, 'cat', sep='-')
+      startPlot(pn, h=5, w=6) # was h=5 w=6
+      plot(d, which='cat', main='', ...)
+      Key(.8, .02)  #.075)
       endPlot()
-      putFig(panel, 'bpplot-prototype',
-             'Prototype box-percentile plot',
-             paste('Prototype box-percentile plot to be used to',
-                   'interpret the following plots'))
+      putFig(panel, pn,
+             paste('Distribution of categorical',longPanel,'variables'),
+             paste('Distribution of categorical',longPanel,'variables.',
+                   'Proportions on the $x$-axis indicate the',
+                   'treatment-specific proportion of subjects in',
+                   'the category shown on the $y$-axis.'))
     }
+    if(any(d$type == 2) || length(Major)) {
+      if(bpPrototype) {
+        startPlot('bpplot-prototype', h=4)
+        bpplt()
+        endPlot()
+        putFig(panel, 'bpplot-prototype',
+               'Prototype box-percentile plot',
+               paste('Prototype box-percentile plot to be used to',
+                     'interpret the following plots'))
+      }
 
-    pn <- paste(panel, 'cont', sep='-')
-    startPlot(paste(pn,'%d',sep=''), h=6, w=6)
-    np <- plot(d, which='con', conType='bp')
-    endPlot()
-    for(i in 1:np)
-      putFig(panel, paste(pn, i, sep=''),
-             paste('Box-percentile plots for continuous',longPanel,'variables',
-                   if(i>1) '(continued)' else ''),
-             paste('Box-percentile plots for continuous ',longPanel,
-                   ' variables',
-                   if(i>1) ' (continued)' else
-                   paste('.  $x$-axes are scaled to the $0.025$ and',
-                         '$0.975$ quantiles when data are pooled',
-                         'over treatments.'),
-                   sep=''))
-  
-    if(cdf) {
-      pn <- paste(panel, 'ecdf', sep='-')
+      pn <- paste(panel, 'cont', sep='-')
       startPlot(paste(pn,'%d',sep=''), h=6, w=6)
-      mfrowSet(length(vars))
-      np <- ecdf(data[vars], group=Treat,
-                 lwd=c(1,2), col=gray(c(0,.7)), q=.5,
-                 label.curves=FALSE)
-      ##         label.curves=list(keys='lines'))
+      np <- plot(d, which='con', conType='bp')
       endPlot()
       for(i in 1:np)
         putFig(panel, paste(pn, i, sep=''),
-               paste('Cumulative distribution plots for',
-                     'continuous', longPanel, 'variables',
-                     if(i>1)'(continued)' else ''),
-               paste('Empirical cumulative distribution plots for ',
-                     'continuous ', longPanel,' variables',
-                     if(i>1)' (continued)' else 
-                     paste('. Reference lines are drawn at',
-                           'treatment-specific median values.',
-                           '\\protect\\treatkey'),
+               paste('Box-percentile plots for continuous',longPanel,'variables',
+                     if(i>1) '(continued)' else ''),
+               paste('Box-percentile plots for continuous ',longPanel,
+                     ' variables',
+                     if(i>1) ' (continued)' else
+                     paste('.  $x$-axes are scaled to the $0.025$ and',
+                           '$0.975$ quantiles when data are pooled',
+                           'over treatments.'),
                      sep=''))
+  
+      if(cdf) {
+        pn <- paste(panel, 'ecdf', sep='-')
+        startPlot(paste(pn,'%d',sep=''), h=6, w=6)
+        mfrowSet(length(vars))
+        np <- ecdf(data[vars], group=Treat,
+                   lwd=c(1,2), col=gray(c(0,.7)), q=.5,
+                   label.curves=FALSE)
+        endPlot()
+        for(i in 1:np)
+          putFig(panel, paste(pn, i, sep=''),
+                 paste('Cumulative distribution plots for',
+                       'continuous', longPanel, 'variables',
+                       if(i>1)'(continued)' else ''),
+                 paste('Empirical cumulative distribution plots for ',
+                       'continuous ', longPanel,' variables',
+                       if(i>1)' (continued)' else 
+                       paste('. Reference lines are drawn at',
+                             'treatment-specific median values.',
+                             '\\protect\\treatkey'),
+                       sep=''))
+      }
     }
   }
 
     panel <- paste('O', panel, sep='')
     form <- as.formula(paste('~', paste(vars,collapse='+')))
-    d <- summary(form, data=data, method='reverse')
-    latex(d, digits=digits, file=paste(panel, 'tex', sep='.'),
+    d <- summary(form, data=data, method='reverse', continuous=continuous)
+    latex(d, digits=digits, file=paste('gentex/',panel, '.tex', sep=''),
         append=append, middle.bold=TRUE,
         caption=paste(lp,'variables'), where='hbp!', ctable=TRUE)
+  if(!pl) return(invisible())
   if(any(d$type == 1)) {
     pn <- paste(panel, 'cat', sep='-')
     startPlot(pn, h=5, w=6) # was h=5 w=6
