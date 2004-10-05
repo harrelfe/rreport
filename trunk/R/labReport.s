930851040a4432f704c49894039a7c2916951f32
harrelfe <harrelfe@b307fd46-ab21-0410-8f3d-bb824a5a1080>
2004-10-05 02:41:35 +0000

    Fixed to include open report, fixed bugs
    git-svn-id: svn+ssh://biostat.mc.vanderbilt.edu/usr/local/svn_repositories/rreport@39 b307fd46-ab21-0410-8f3d-bb824a5a1080

--- trunk/R/labReport.s
+++ trunk/R/labReport.s
@@ -2,14 +2,13 @@
 labReport <- function(data, vars, panel, treat, time, times,
                       longPanel=panel, h=6.5, w=6.5,
                       diffs=FALSE, cdf=FALSE, tables=TRUE,
-                      digits=3, append=TRUE, clearPlots=FALSE) {
+                      digits=3, append=FALSE, clearPlots=FALSE) {
 
   vars  <- unlist(vars)
   Treat <- data[[treat]]
   Time  <- data[[time]]
 
-  plots <- vector('list', length(vars))
-
+  makerep <- function(Treat, panel) {
   g <- function(y) {
     y <- sort(y[!is.na(y)])
     n <- length(y)
@@ -21,15 +20,16 @@ labReport <- function(data, vars, panel, treat, time, times,
     var.med <- ((w/1.96)^2)/4      ## Approximate variance of median
     c(median=qu[1], q1=qu[2], q3=qu[3], variance=var.med)
   }
+  Treat <- as.factor(Treat)
   trlev <- levels(Treat)
   if(!length(trlev)) stop(paste(treat,'is not a factor variable'))
-  if(length(trlev) != 2) stop('only handles two treatments')
+  if(length(trlev) > 2) stop('only handles one or two treatments')
 
   i <- 0
   nv <- length(vars)
   h <- w <- 4.5*(nv < 7) + 5.5*(nv >= 7 & nv <=9) + 6.5*(nv > 9)
 
-  startPlot(paste('lab-',panel,'%d',sep=''), h=h, w=w)
+  startPlot(paste(panel,'%d',sep=''), h=h, w=w)
   omf <- mfrowSet(nv)
   mf <- par('mfrow')
 
@@ -37,8 +37,9 @@ labReport <- function(data, vars, panel, treat, time, times,
     i <- i + 1
     z <- summarize(y, llist(Treat,Time), g)
     curves <- vector('list', length(trlev)*3)
-    names(curves) <- c(paste(trlev[1],c('','q1','q3'),sep=''),
-                       paste(trlev[2],c('','q1','q3'),sep=''))
+    names(curves) <- if(length(trlev)==1) c('','q1','q3') else
+    c(paste(trlev[1],c('','q1','q3'),sep=''),
+      paste(trlev[2],c('','q1','q3'),sep=''))
     
     j <- 0
     for(tr in trlev) {
@@ -58,15 +59,17 @@ labReport <- function(data, vars, panel, treat, time, times,
     ## For each time get an approximate half-width of 0.95 CL for
     ## difference in median y between treatments.  Also save
     ## midpoint of two medians for plotting vertical half-interval.
-    tt <- sort(unique(z$Time))
-    j <- 0
-    for(tim in tt) {
-      j <- j + 1
-      s <- z$Time==tim
-      midpt  <- mean(z$y[s])
-      clhalf <- 1.96*sqrt(sum(z$variance[s]))
-      lines(c(tim,tim), c(midpt-clhalf/2, midpt+clhalf/2),
-            lwd=0.5, col=gray(0.7))
+    if(length(trlev)==2) {
+      tt <- sort(unique(z$Time))
+      j <- 0
+      for(tim in tt) {
+        j <- j + 1
+        s <- z$Time==tim
+        midpt  <- mean(z$y[s])
+        clhalf <- 1.96*sqrt(sum(z$variance[s]))
+        lines(c(tim,tim), c(midpt-clhalf/2, midpt+clhalf/2),
+              lwd=0.5, col=gray(0.7))
+      }
     }
   }
   endPlot()
@@ -74,7 +77,15 @@ labReport <- function(data, vars, panel, treat, time, times,
   for(npage in 1:ceiling(nv/prod(mf))) {
     cap <- paste('Quartiles of ',longPanel,' variables over time',
                  if(npage > 1)' (continued)' else '', sep='')
-    putFig(panel,paste('lab-',panel,npage,sep=''),
+    confcap <- if(length(trlev) == 1) NULL else
+                 paste(' Vertical bars indicate half-widths of',
+                   ' approximate 0.95 confidence intervals for',
+                   ' differences in medians.',
+                   ' When the distance between two medians',
+                   ' exceeds the length of the bar, differences are',
+                   ' significant at approximately the 0.05 level.',
+                   ' \\protect\\treatkey', sep='')
+    putFig(panel,paste(panel,npage,sep=''),
            cap,
            if(npage == 1)
            paste(cap,
@@ -83,13 +94,7 @@ labReport <- function(data, vars, panel, treat, time, times,
                  ' Thicker middle lines depict medians.',
                  ' $y$-axis is scaled to the pooled $5^{th}$',
                  ' and $95^{th}$ quantiles.',
-                 ' Vertical bars indicate half-widths of',
-                 ' approximate 0.95 confidence intervals for',
-                 ' differences in medians.',
-                 ' When the distance between two medians',
-                 ' exceeds the length of the bar, differences are',
-                 ' significant at approximately the 0.05 level.',
-                 ' \\protect\\treatkey',
+                 confcap,
                  sep='') else NULL,
            append=npage > 1)
   }
@@ -104,9 +109,13 @@ labReport <- function(data, vars, panel, treat, time, times,
     h <- w <- 4.5*(nt < 7) + 5.5*(nt >= 7 & nt <=9) + 6.5*(nt > 9)
     for(v in data[vrs]) {
       i <- i + 1
-      fn <- paste('lab-',panel,'-ecdf-',vrs[i],sep='')
+      fn <- paste(panel,'-ecdf-',vrs[i],sep='')
       startPlot(fn, trellis=TRUE, h=h, w=w)
       d <- data.frame(v,tf,Treat,Time)[Time %in% times,]
+      if(length(trlev)==1)
+      print(ecdf( ~ v | tf, data=d, q=.5,
+                 label.curve=FALSE,
+                 as.table=TRUE)) else
       print(ecdf( ~ v | tf, groups=Treat, data=d,
                  lty=c(1,1), lwd=c(1,2), col=gray(c(0,.7)), q=.5,
                  label.curve=FALSE,
@@ -116,26 +125,32 @@ labReport <- function(data, vars, panel, treat, time, times,
       endPlot()
       putFig(panel, fn,
              paste('Cumulative distribution of',label(v),
-                   'by treatment over time'),
+                   if(length(trlev) > 1) 'by treatment over time'),
              paste('Empirical cumulative distribution function of',
-                   label(v),'by treatment over time. \\protect\\treatkey'))
+                   label(v),
+                   if(length(trlev) > 1) 'by treatment over time. \\protect\\treatkey'))
     }
   }
   
   if(!tables) return(invisible())
 
-  form <- as.formula(paste(treat,paste(vars,collapse='+'),sep='~'))
+  form <- as.formula(paste(if(length(trlev)==1)'' else treat,
+                           paste(vars,collapse='+'),sep='~'))
 
   if(clearPlots)
-    cat('\\clearpage\n', file=paste(panel,'tex',sep='.'), append=TRUE)
+    cat('\\clearpage\n', file=paste('gentex/',panel,'.tex',sep=''),
+        append=TRUE)
   
   for(x in times) {
     s <- summary(form, data=data[Time==x,], method='reverse',
-                 test=TRUE)
+                 test=length(trlev) > 1)
     w <- latex(s, file=paste('gentex/',panel,'.tex',sep=''), append=TRUE,
                middle.bold=TRUE, title='',
                caption=paste(longPanel,'Data at',label(Time),x),
                prtest='P', digits=digits, where='hbp!', ctable=TRUE)
   }
+}
+  makerep(Treat, panel)
+  makerep(rep('', length(Treat)), paste('O',panel,sep=''))
   invisible()
 }
