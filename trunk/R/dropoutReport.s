e360f84528bbe945a89e274419a0b4887f895b68
harrelfe <harrelfe@b307fd46-ab21-0410-8f3d-bb824a5a1080>
2005-02-26 23:36:26 +0000

    Fixed bug, added ... argument to pass to survplot
    git-svn-id: svn+ssh://biostat.mc.vanderbilt.edu/usr/local/svn_repositories/rreport@61 b307fd46-ab21-0410-8f3d-bb824a5a1080

--- trunk/R/dropoutReport.s
+++ trunk/R/dropoutReport.s
@@ -1,27 +1,32 @@
 ## $Id$
 dropoutReport <- function(d.dropout, dropout, treat,
-                          time.inc=NULL, ylim=c(0,1)) {
+                          time.inc=NULL, ylim=c(0,1), ...) {
   require('survival')
   require('Design')
   S <- if(length(dropout)) Surv(d.dropout, dropout) else
       Surv(d.dropout)
   startPlot('Odropout', h=4)
-  d <- data.frame(S, treat)
+  d <- data.frame(treat)
+  d$S <- S
   f <- survfit(S, data=d)
   yl <- 'Fraction Remaining in Study'
   lwd <- c(1,2); lty=c(1,1); col=gray(c(0,.7))
   if(length(time.inc))
     survplot(f, time.inc=time.inc, n.risk=TRUE, conf='none', ylab=yl,
-      lwd=lwd, lty=lty, col=col)
-      else survplot(f, conf='none', ylab=yl, lwd=lwd, lty=lty, col=col)
+      lwd=lwd, lty=lty, col=col, ylim=ylim, ...) else
+      survplot(f, conf='none', ylab=yl, lwd=lwd, lty=lty, col=col,
+               ylim=ylim, n.risk=TRUE, ...)
+               
   endPlot()
   startPlot('dropout', h=4)
   f <- survfit(S ~ treat, data=d)
   if(length(time.inc))
     survplot(f, time.inc=time.inc, n.risk=TRUE, conf='none', ylab=yl,
-             lwd=lwd, lty=lty, col=col, label.curves=FALSE)
-      else survplot(f, conf='none', ylab=yl,
-                    lwd=lwd, lty=lty, col=col, label.curves=FALSE)
+             lwd=lwd, lty=lty, col=col, label.curves=FALSE, ylim=ylim,
+             ...)
+      else survplot(f, conf='none', ylab=yl, n.risk=TRUE,
+                    lwd=lwd, lty=lty, col=col, label.curves=FALSE,
+                    ylim=ylim, ...)
   endPlot()
   for(w in c('dropout','Odropout'))
     putFig(w, w,
