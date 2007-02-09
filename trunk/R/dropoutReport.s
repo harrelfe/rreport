d29f6bf7a0a32e5fbe5644ed8bb35fda69aae6ae
edensk <edensk@b307fd46-ab21-0410-8f3d-bb824a5a1080>
2007-02-09 18:38:52 +0000

    git-svn-id: svn+ssh://biostat.mc.vanderbilt.edu/usr/local/svn_repositories/rreport@155 b307fd46-ab21-0410-8f3d-bb824a5a1080

--- trunk/R/dropoutReport.s
+++ trunk/R/dropoutReport.s
@@ -1,11 +1,12 @@
 ## $Id$
 dropoutReport <- function(d.dropout, dropout, treat,
-                          time.inc=NULL, ylim=c(0,1), ...) {
+                          time.inc=NULL, ylim=c(0,1), panel="dropout", ...) {
   require('survival')
   require('Design')
   S <- if(length(dropout)) Surv(d.dropout, dropout) else
       Surv(d.dropout)
-  startPlot('Odropout', h=4)
+  openPanel <- paste("O", panel, sep="")
+  startPlot(openPanel, h=4)
   d <- data.frame(treat)
   d$S <- S
   f <- survfit(S, data=d)
@@ -18,7 +19,7 @@ dropoutReport <- function(d.dropout, dropout, treat,
                ylim=ylim, n.risk=TRUE, ...)
                
   endPlot()
-  startPlot('dropout', h=4)
+  startPlot(panel, h=4)
   f <- survfit(S ~ treat, data=d)
   if(length(time.inc))
     survplot(f, time.inc=time.inc, n.risk=TRUE, conf='none', ylab=yl,
@@ -28,10 +29,10 @@ dropoutReport <- function(d.dropout, dropout, treat,
                     lwd=lwd, lty=lty, col=col, label.curves=FALSE,
                     ylim=ylim, ...)
   endPlot()
-  for(w in c('dropout','Odropout'))
+  for(w in c(panel,openPanel))
     putFig(w, w,
            'Distribution of time until dropout',
-           if(w=='dropout')
+           if(w==panel)
            'Distribution of time until dropout. \\protect\\treatkey'
            else 'Distribution of time until dropout',
            append=FALSE)
