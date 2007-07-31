db3858564802c3697bb0c76fc5141af489c87daa
harrelfe <harrelfe@b307fd46-ab21-0410-8f3d-bb824a5a1080>
2007-07-31 15:50:36 +0000

    git-svn-id: svn+ssh://biostat.mc.vanderbilt.edu/usr/local/svn_repositories/rreport@170 b307fd46-ab21-0410-8f3d-bb824a5a1080

--- trunk/R/repVarclus.s
+++ trunk/R/repVarclus.s
@@ -2,7 +2,6 @@
 repVarclus <- function(data, time, times=sort(unique(time)),
                        nmin=10)
 {
-
   g <- function(y, nmin)
     {
       y <- table(oldUnclass(y))
@@ -16,27 +15,32 @@ repVarclus <- function(data, time, times=sort(unique(time)),
           'are correlated with one another, as measured by the square',
           'of the Spearman $\\rho$ rank correlation coefficient',
           'computed on all pairs of variables.  Variables connected on',
-          'lower branches are more highly correlated with one another.\n',
+          'lower branches are more highly correlated with one another.',
+          'Variables missing in more than 0.75 of the observations or',
+          'categorical variables having more than 20 levels are ignored.',
+          'Categories less than 0.1 prevalent are pooled with other rare categories.\n',
           file='gentex/Ovarclus.tex')
       startPlot('Ovarclus', h=4)
       d <- data[time==0,]
       r <- sapply(d, g, nmin=nmin)
       d <- d[r]
-      v <- varclus(~., data=d)
+      v <- varclus(~., data=d, fracmiss=.75, maxlevels=20, minprev=.1)
       plot(v)
       putFig('Ovarclus', 'Ovarclus',
              'Clustering of variables at baseline',
              append=TRUE)
       endPlot()
     }
-
   cat('Variable clustering diagrams are shown in the',
-      if(length(times) > 1)'figures' else 'figure',
-      'which follows.  Variables are grouped according to how they are',
+      if(length(times) > 1)'figures that follow.' else 'figure that follows.', 
+      'Variables are grouped according to how they are',
       'correlated with one another, as measured by the square of the',
       'Spearman $\\rho$ rank correlation coefficient computed on all',
       'pairs of variables.  Variables connected on',
-      'lower branches are more highly correlated with one another.\n',
+      'lower branches are more highly correlated with one another.',
+      'Variables missing in more than 0.75 of the observations or',
+      'categorical variables having more than 20 levels are ignored.',
+      'Categories less than 0.1 prevalent are pooled with other rare categories.\n',
       file='gentex/varclus.tex')
   startPlot('varclus%d', h=4)
   i <- 0
@@ -46,7 +50,7 @@ repVarclus <- function(data, time, times=sort(unique(time)),
       d <- data[time==x,]
       r <- sapply(d, g, nmin=nmin)
       d <- d[r]
-      v <- varclus(~., data=d)
+      v <- varclus(~., data=d, fracmiss=.75, maxlevels=20, minprev=.1)
       plot(v)
       title(paste(label(time), x))
       putFig('varclus', paste('varclus',i,sep=''),
