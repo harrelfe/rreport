88c366344b72c876530fb9ff548cddc897430c94
harrelfe <harrelfe@b307fd46-ab21-0410-8f3d-bb824a5a1080>
2007-07-31 15:19:54 +0000

    added a missing gentex/, neatened code
    git-svn-id: svn+ssh://biostat.mc.vanderbilt.edu/usr/local/svn_repositories/rreport@168 b307fd46-ab21-0410-8f3d-bb824a5a1080

--- trunk/R/repVarclus.s
+++ trunk/R/repVarclus.s
@@ -1,31 +1,34 @@
 ## $Id$
 repVarclus <- function(data, time, times=sort(unique(time)),
-                       nmin=10) {
+                       nmin=10)
+{
 
-  g <- function(y, nmin) {
-    y <- table(oldUnclass(y))
-    length(y) > 3 || (length(y) > 1 && sort(y)[length(y)-1] > nmin)
-  }
+  g <- function(y, nmin)
+    {
+      y <- table(oldUnclass(y))
+      length(y) > 3 || (length(y) > 1 && sort(y)[length(y)-1] > nmin)
+    }
 
-  if(any(times==0)) {
-    cat('A variable clustering diagram is shown in the figure',
-        'which follows.  Variables are grouped according to how they',
-        'are correlated with one another, as measured by the square',
-        'of the Spearman $\\rho$ rank correlation coefficient',
-        'computed on all pairs of variables.  Variables connected on',
-        'lower branches are more highly correlated with one another.\n',
-        file='Ovarclus.tex')
-    startPlot('Ovarclus', h=4)
-    d <- data[time==0,]
-    r <- sapply(d, g, nmin=nmin)
-    d <- d[r]
-    v <- varclus(~., data=d)
-    plot(v)
-    putFig('Ovarclus', 'Ovarclus',
-           'Clustering of variables at baseline',
-           append=TRUE)
-    endPlot()
-  }
+  if(any(times==0))
+    {
+      cat('A variable clustering diagram is shown in the figure',
+          'which follows.  Variables are grouped according to how they',
+          'are correlated with one another, as measured by the square',
+          'of the Spearman $\\rho$ rank correlation coefficient',
+          'computed on all pairs of variables.  Variables connected on',
+          'lower branches are more highly correlated with one another.\n',
+          file='gentex/Ovarclus.tex')
+      startPlot('Ovarclus', h=4)
+      d <- data[time==0,]
+      r <- sapply(d, g, nmin=nmin)
+      d <- d[r]
+      v <- varclus(~., data=d)
+      plot(v)
+      putFig('Ovarclus', 'Ovarclus',
+             'Clustering of variables at baseline',
+             append=TRUE)
+      endPlot()
+    }
 
   cat('Variable clustering diagrams are shown in the',
       if(length(times) > 1)'figures' else 'figure',
@@ -34,23 +37,23 @@ repVarclus <- function(data, time, times=sort(unique(time)),
       'Spearman $\\rho$ rank correlation coefficient computed on all',
       'pairs of variables.  Variables connected on',
       'lower branches are more highly correlated with one another.\n',
-      file='varclus.tex')
+      file='gentex/varclus.tex')
   startPlot('varclus%d', h=4)
   i <- 0
-  for(x in times) {
-    i <- i + 1
-    d <- data[time==x,]
-    r <- sapply(d, g, nmin=nmin)
-    d <- d[r]
-    v <- varclus(~., data=d)
-    plot(v)
-    title(paste(label(time), x))
-    putFig('varclus', paste('varclus',i,sep=''),
-           paste('Clustering of variables at',label(time),x),
-           append=TRUE)
-  }
+  for(x in times)
+    {
+      i <- i + 1
+      d <- data[time==x,]
+      r <- sapply(d, g, nmin=nmin)
+      d <- d[r]
+      v <- varclus(~., data=d)
+      plot(v)
+      title(paste(label(time), x))
+      putFig('varclus', paste('varclus',i,sep=''),
+             paste('Clustering of variables at',label(time),x),
+             append=TRUE)
+    }
   endPlot()
   
   invisible()
 }
- 
